8d244a93bdb27565994bdf2265427951f03a9ceb
lyheu <lyheunpha-ly@hotmail.fr>
2023-06-02 14:43:34 +0200

    introduction d'une fonctionnalité permettant d'afficher la tâche complète lors du survol de la souris

--- projet-todolist/src/app/list/list.component.ts
+++ projet-todolist/src/app/list/list.component.ts
@@ -9,6 +9,8 @@ import { Item } from './interface';
 export class ListComponent {
 
   item : Item = {title:"",status:false};
+  msg: string = "Tâche effectué.";
+  mouseOver: number =-1;
 
   listItem: Item[] = [
     {title:"Balayer le salon.",status:false},
@@ -21,26 +23,16 @@ export class ListComponent {
     {title:"Récupérer les enfants à l'école.",status:false}
   ]
 
-  msg: string = "Tâche effectué.";
-
   addNewItem(newItem: string){
     this.item = {title:newItem,status: false};
-    // this.item.title = newItem;
     this.listItem.push(this.item);
   }
 
   updateItem = (index: number, selected: boolean) : void => {
     this.listItem[index].status = selected;
-    // this.listItem[index] = {title:this.listItem[index].title, status:selected};
   }
 
   methodeA = (itemList: Item[]) : void => {
-    // for(let i: number = 0 ; i<this.listItem.length ; i++) {
-    //   itemList.push(this.listItem[i]);
-    // }
-
-    // itemList =  this.listItem;
-
     this.listItem = itemList;
   }
 
